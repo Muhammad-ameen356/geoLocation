80a2dd5cc0786c3b08c612d3012789ac4526fd3e
muhammad ameen <muhammadameenh1@gmail.com>
2021-10-08 21:15:34 +0500

    Geo Location

---
+++ app.js
@@ -0,0 +1,23 @@
+let x = document.getElementById("demo");
+let google_map = document.getElementById('google_map');
+
+const getCord = () => {
+    console.log("ameen");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+const showPosition = (position)=> {
+    let lat     = position.coords.latitude,
+        long    = position.coords.longitude,
+        coords  = lat + ', ' + long;
+
+    x.innerHTML = "Latitude: " + lat + 
+    "<br>Longitude: " + long;
+
+    document.getElementById('google_map').setAttribute('src', 'https://maps.google.co.uk/?q=' + coords + '&z=60&output=embed')
+
+  }

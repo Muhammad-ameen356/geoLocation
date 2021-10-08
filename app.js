2bb83837219d1849ef3e290e53bba7de0ccf5922
muhammad ameen <muhammadameenh1@gmail.com>
2021-10-08 21:47:24 +0500

    Geo Location

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const getCord = () => {
     }
 }
 
-const showPosition = (position)=> {
-    let lat     = position.coords.latitude,
-        long    = position.coords.longitude,
-        coords  = lat + ', ' + long;
+const showPosition = (position) => {
+    let lat = position.coords.latitude,
+        long = position.coords.longitude,
+        coords = lat + ', ' + long;
 
-    x.innerHTML = "Latitude: " + lat + 
-    "<br>Longitude: " + long;
+    x.innerHTML = "Latitude: " + lat +
+        "<br>Longitude: " + long;
 
-    document.getElementById('google_map').setAttribute('src', 'https://maps.google.co.uk/?q=' + coords + '&z=60&output=embed')
+    document.getElementById('google_map').setAttribute('src', 'https://maps.google.co.uk/?q=' + coords + '&z=16&output=embed')
 
-  }
+}

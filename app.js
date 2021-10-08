1903d5e663e19a77e990f6fa6b747304da032358
muhammad ameen <muhammadameenh1@gmail.com>
2021-10-08 22:23:23 +0500

    Geo Location

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ let google_map = document.getElementById('google_map');
 const getCord = () => {
     console.log("ameen");
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        alert("Geolocation is not supported by this browser.");
     }
 }
 
 const showPosition = (position) => {
+    console.log(position);
     let lat = position.coords.latitude,
         long = position.coords.longitude,
         coords = lat + ', ' + long;
@@ -19,5 +20,13 @@ const showPosition = (position) => {
         "<br>Longitude: " + long;
 
     document.getElementById('google_map').setAttribute('src', 'https://maps.google.co.uk/?q=' + coords + '&z=16&output=embed')
+}
 
+const showError = (error) => {
+    console.log(error);
+    if (error.code == error.PERMISSION_DENIED) {
+        console.log(error.message);
+    } else if (error.code == error.POSITION_UNAVAILABLE) {
+        console.log(error.message);
+    }
 }
